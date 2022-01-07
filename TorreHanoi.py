d74aa3ee40aa3a492d2a93fedadb52f185a22195
PedroBusin1 <97306796+PedroBusin1@users.noreply.github.com>
2022-01-07 19:12:58 -0300

    Add files via upload

---
+++ TorreHanoi.py
@@ -0,0 +1,13 @@
+"""
+Descrição do jogo
+"""
+
+# Document properties
+__author__ = ['Pedro_Henrique_Busin_Cambruzzi', 'Pedro_Henrique_de_Miranda_Pinto']
+__copyright__ = 'Copyright_2022'
+__credits__ = __author__
+__license__ = 'GPL'
+__version__ = '1.0.0'
+__maintainer__ = 'Pedro Henrique Busin Cambruzzi'
+__email__ = 'pedrobusin@poli.ufrj.br'
+__status__ = 'Production'
